61197904390c888b1995a4237a31e56b07159603
karto-yokesh <yokesh.r@kartoffel.in>
2024-06-11 15:48:19 +0530

    Day5

---
+++ Day 5/Demo.js
@@ -0,0 +1,22 @@
+
+var box1 = document.getElementById("num1");
+var box2 = document.getElementById("num2");
+
+var result = document.getElementById("result");
+
+console.log(box1.value);
+console.log(box2.value);
+
+var box1value = Number(box1.value)
+var box2value = Number(box2.value)
+
+
+var total = box1value+box2value;
+function addition()
+{
+    result.textContent = total
+
+}
+
+
+
